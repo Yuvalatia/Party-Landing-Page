fa538a08ba4e198a2ccd65532e148c9a3e2a790c
yuvalatia <yodaatia1995@gmail.com>
2019-05-16 13:51:13 +0300

    added table hover effects

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-
+/* --- Image Gallery --- */
 // VARS - usable
 var pics = ["imgs/pic1.jpg","imgs/pic2.jpg","imgs/pic3.jpg"]
 var counter = 0;
@@ -12,6 +12,7 @@ var img = document.getElementById("pict");
 document.getElementById("left-arrow").addEventListener("click",function () {
     console.log(counter);
     switch(counter){
+        
         case 0:
             counter = 2;
             img.style.backgroundImage = "url("+ pics[2]+ ")";
@@ -44,3 +45,24 @@ document.getElementById("right-arrow").addEventListener("click",function () {
     }
 
 });
+    /* --- Text hover --- */
+    var mainText = document.getElementsByClassName("content")[0];
+    mainText.addEventListener("mouseover",function () {
+        mainText.style.fontSize = "17px";
+    });
+    mainText.addEventListener("mouseout",function () {
+        mainText.style.fontSize = "16px";
+    });
+
+    /* --- Table hover --- */
+    table = document.getElementsByTagName("td");
+    for(let i=0; i<table.length; i++){
+
+        table[i].addEventListener("mouseover", function () {
+            table[i].style.backgroundColor = "white";
+        });
+        table[i].addEventListener("mouseout", function () {
+            table[i].style.backgroundColor = "";
+        });
+
+    }
